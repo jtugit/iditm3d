958690ca2e5c0cbce5b2f8b1a88ef1d015eaf1ad
Jiannan Tu <jiannan_tu@uml.edu>
2022-09-24 20:48:26 -0400

    Complete fluxes but large max speed dominant

--- iditm3d.sh
+++ iditm3d.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mpiexec -n $1 ./iditm3d -ksp_type fgmres -pc_tyep jacobi -ts_rtol 1.0e-8 -ts_atol 1.0e-8 \
+mpiexec -n $1 ./iditm3d -ts_type cn -ksp_type fgmres -pc_tyep jacobi -ts_rtol 1.0e-8 -ts_atol 1.0e-8 \
 -ts_monitor -ts_max_snes_failures -1
